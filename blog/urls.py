701e2f65d26d640f74bac13a6693e46c17e6e139
guillermo <memopc01@gmail.com>
2019-06-12 18:32:42 -0500

    Actualizacion Seguridad

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    
 
 ]
