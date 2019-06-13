f61d8ebb2868f3446bf0116f51ce42a39da0a78b
guillermo <memopc01@gmail.com>
2019-06-13 12:14:53 -0500

    Toques finales

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    approved_comment = models.BooleanField(default=False)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,6 +18,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
 
-# Create your models here.
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
